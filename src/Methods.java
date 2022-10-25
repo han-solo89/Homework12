63cc20f53984384a9766310bc8fc510428f257da
igorbelow <lord.below@yandex.ru>
2022-10-26 02:12:59 +0300

    методы

---
+++ src/Methods.java
@@ -0,0 +1,69 @@
+import java.time.LocalDate;
+
+public class Methods {
+
+    public static void printIsLeapYear(int year) {
+        boolean yearIsLeap = isLeap(year);
+        printIsLeapYearResult(year, yearIsLeap);
+
+
+    }
+
+    public static boolean isLeap(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
+    public static void printIsLeapYearResult(int year, boolean yearIsLeap) {
+        if (yearIsLeap) {
+            System.out.println(year + "високосный");
+        } else {
+            System.out.println(year + "не является високосным");
+        }
+    }
+
+    public static void recommendApplicationVersion(int clientOS, int deviceYear) {
+        boolean deviceIsOld = isDeviceOld(deviceYear);
+        System.out.print("установите");
+        if (deviceIsOld) {
+            System.out.print("life");
+        }
+        System.out.print("версия для");
+        if (clientOS == 0) {
+            System.out.println("iOS");
+        } else {
+            System.out.println("Android");
+        }
+    }
+    private static boolean isDeviceOld(int deviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        return deviceYear <= currentYear;
+
+    }
+
+    public static void calculateDeliveryDays(int deliveryDistance) {
+        int result = 1;
+        if (deliveryDistance > 20) {
+            result++;
+        }
+        if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            result++;
+        } else {
+            System.out.println("Невзможно расчитать срок действия");
+        }
+        System.out.println("Потребуется дней : " + result);
+    }
+
+
+    public static void main(String[] args){
+    }
+}
+
+
+
+
+
+
+
+
+
+
